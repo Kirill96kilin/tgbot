710e4ea78b9c075f1c9f838103c819313a6a664e
Kirill96kilin <Kirill3plkill@gmail.com>
2024-07-13 15:24:49 +0700

    fix

--- keyb.py
+++ keyb.py
@@ -6,9 +6,6 @@ def generate_options_keyboard(answer_options, right_answer):
     builder = InlineKeyboardBuilder()
 
     for option in answer_options:
-
-        #Метод калл - менять
-        
         builder.add(types.InlineKeyboardButton(
             text=option,
             callback_data="right_answer" if option == right_answer else "wrong_answer")
